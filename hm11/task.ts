d97169243da9c56c2f5dc274c51e031cda53da9b
konstantin <k.hromakovskiy@yandex.ru>
2026-05-28 18:37:29 +0300

    add hm11

---
+++ hm11/task.ts
@@ -0,0 +1,151 @@
+interface StackPointerI {
+  deref(): ArrayBufferLike;
+  change(value: ArrayBufferLike): void;
+}
+
+interface HeapPointerI extends StackPointerI {
+  free(): void;
+}
+
+interface MemoryI {
+  push(value: ArrayBuffer): StackPointerI;
+  pop(): void;
+  alloc(size: number): HeapPointerI;
+}
+
+class StackPointer implements StackPointerI {
+  metadata: Int32Array;
+  bytes: Uint8Array;
+  constructor(payload: Uint8Array, metadata: Int32Array) {
+    this.bytes = payload;
+    this.metadata = metadata;
+  }
+  deref(): ArrayBufferLike {
+    return this.bytes.slice().buffer;
+  }
+  change(value: ArrayBuffer): void {
+    const valueBytes = new Uint8Array(value);
+    const valueByteLenth = valueBytes.byteLength;
+    if (valueByteLenth > this.metadata[0])
+      throw new Error("value bigger then memory item");
+
+    this.bytes.fill(0);
+    this.bytes.set(valueBytes);
+  }
+}
+
+class HeapPointer extends StackPointer {
+  free() {
+    if (this.metadata[0] < 0) throw new Error("❌ Error: double free detected");
+
+    this.metadata[0] = -this.metadata[0];
+  }
+}
+
+class Memory {
+  #metaDataByteLenth = 4;
+  stack: Uint8Array;
+  heap: Uint8Array;
+  buffer: ArrayBufferLike;
+  stackView: DataView;
+  heapView: DataView;
+  stackCursor = 0;
+  constructor(totalMemory: number, options: { stackMemoryValue: number }) {
+    const { stackMemoryValue } = options;
+    this.buffer = new ArrayBuffer(totalMemory);
+    const bytes = new Uint8Array(this.buffer, 0);
+    this.stackView = new DataView(this.buffer, 0, stackMemoryValue);
+    this.stack = bytes.subarray(0, stackMemoryValue);
+    this.heap = bytes.subarray(stackMemoryValue);
+    this.heapView = new DataView(this.buffer, stackMemoryValue);
+    const heapSize = totalMemory - stackMemoryValue;
+    //Если число размера блока положительное - блок занят, если отрицательное - свободен
+    this.heapView.setInt32(0, -(heapSize - 4));
+  }
+
+  push(value: ArrayBuffer): StackPointerI {
+    const valueBytes = new Uint8Array(value);
+    const valueByteLenth = valueBytes.byteLength;
+    const nextCursorOffset = valueByteLenth + this.#metaDataByteLenth;
+
+    if (this.stackCursor + nextCursorOffset > this.stack.byteLength)
+      throw new Error("Stack over flow");
+
+    this.stack.set(valueBytes, this.stackCursor);
+    this.stackView.setUint32(this.stackCursor + valueByteLenth, valueByteLenth);
+    this.stackCursor += nextCursorOffset;
+    const payload = this.stack.subarray(
+      this.stackCursor - nextCursorOffset,
+      this.stackCursor - this.#metaDataByteLenth,
+    );
+    const metadata = new Int32Array(
+      this.buffer,
+      payload.byteOffset + payload.byteLength,
+      1,
+    );
+    metadata[0] = valueByteLenth;
+
+    return new StackPointer(payload, metadata);
+  }
+  pop() {
+    if (this.stackCursor === 0) throw new Error("Stack underflow");
+    const lastItemLenth = this.stackView.getInt32(
+      this.stackCursor - this.#metaDataByteLenth,
+    );
+    this.stackCursor -= lastItemLenth + this.#metaDataByteLenth;
+  }
+  alloc(size: number): HeapPointerI {
+    return searchFreeMemory(this.heap, size, 0);
+  }
+}
+
+function searchFreeMemory(
+  heapBytes: Uint8Array,
+  seartchebleSize: number,
+  offset: number,
+) {
+  if (heapBytes.byteLength < offset)
+    throw new Error("free memory is not founded");
+  const bytesView = new DataView(heapBytes.buffer, heapBytes.byteOffset);
+  const blockSize = bytesView.getInt32(offset);
+  const isFree = blockSize < 0;
+
+  if (isFree && Math.abs(blockSize) >= seartchebleSize) {
+    const blockView = new DataView(
+      heapBytes.buffer,
+      heapBytes.byteOffset,
+      heapBytes.byteLength,
+    );
+    blockView.setInt32(offset, seartchebleSize);
+
+    const nextHeaderOffset = offset + 4 + seartchebleSize;
+    const freePayloadSize = Math.abs(blockSize);
+    const remainingPayloadSize = freePayloadSize - seartchebleSize - 4;
+    if (remainingPayloadSize > 0) {
+      blockView.setInt32(nextHeaderOffset, -remainingPayloadSize);
+    }
+    return new HeapPointer(
+      heapBytes.subarray(offset + 4, offset + 4 + seartchebleSize),
+      new Int32Array(heapBytes.buffer, heapBytes.byteOffset + offset, 1),
+    );
+  }
+  return searchFreeMemory(
+    heapBytes,
+    seartchebleSize,
+
+    offset + 4 + Math.abs(blockSize),
+  );
+}
+
+const mem = new Memory(128, { stackMemoryValue: 32 });
+
+const value1 = new Uint8Array([4, 5, 2, 6]);
+const value2 = new Uint8Array([7, 5, 7]);
+
+const pointer1 = mem.push(value1.buffer);
+mem.push(value2.buffer);
+pointer1.change(value2.buffer);
+mem.pop();
+
+const alloc = mem.alloc(5);
+console.log("alloc", alloc.deref());
